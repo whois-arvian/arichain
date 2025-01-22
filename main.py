e48a62a0d51bb2d96015cbc0684637bd29591292
Whois Arvian <arviannewbie473@gmail.com>
2025-01-22 16:29:56 +0700

    up

--- main.py
+++ main.py
@@ -381,6 +381,11 @@ def generate_email(domain):
     return email
 
 async def get_otp(email, domain):
+    cookies = {
+        'embx': f'[%22{email}%22]',
+        'surl': f'{domain}/{email.split("@")[0]}'
+    }
+    
     for inbox_num in range(1, 10):  # Checking inbox from 1 to 9
         attempt = 0
         while attempt < max_retries:
@@ -390,13 +395,10 @@ async def get_otp(email, domain):
                 # Send GET request to the inbox
                 response = requests.get(
                     f'https://emailfake.com/inbox{inbox_num}/',
-                    headers={
-                        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                        'accept-encoding': 'gzip, deflate, br, zstd',
-                        'accept-language': 'en-US,en;q=0.9',
-                        'cookie': f'embx=["{email}@{domain}"]',  # Adjust this according to your cookie needs
-                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
-                    }
+                    headers=get_headers(),
+                    cookies=cookies,
+                    impersonate="chrome110",
+                    timeout=120
                 )
 
                 # Parse the HTML response
