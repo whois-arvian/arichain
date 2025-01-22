b1e00b6d44f1b97513613d9e247abe1c1fbc1818
Whois Arvian <arviannewbie473@gmail.com>
2025-01-22 16:18:16 +0700

    up

--- main.py
+++ main.py
@@ -389,7 +389,7 @@ async def get_otp(email, domain):
 
                 # Send GET request to the inbox
                 response = requests.get(
-                    f'https://generator.email/{email}/',
+                    f'https://generator.email/inbox{inbox_num}/',
                     headers={
                         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                         'accept-encoding': 'gzip, deflate, br, zstd',
